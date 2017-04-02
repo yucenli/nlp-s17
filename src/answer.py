beccc51161372904e41ed7f265ddc63fbcb4e146
Jun Yong Go <Jun@Juns-MBP-2.wv.cc.cmu.edu>
2017-04-02 13:29:47 -0400

    no changes

--- src/answer.py
+++ src/answer.py
@@ -1,55 +1,225 @@
-import ner
-import txt_ner
-from nltk.stem.wordnet import WordNetLemmatizer
+import spacy
+import math
+import os
+import re
+import sys
+from collections import Counter, defaultdict
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import linear_kernel
+
+nlp = spacy.load('en')
+
+reload(sys)
+sys.setdefaultencoding('utf8')
 
 
-def filterSentencesByWord(sentences, taggedSentences, mWord):
-    filteredSentences = []
-    filteredTaggedSentences = []
-    for sentence, tSentence in zip(sentences, taggedSentences):
-        for wordTag in tSentence:
-            if wordTag[0] == mWord:
-                filteredSentences.append(sentence)
-                filteredTaggedSentences.append(tSentence)
-                break
-    return (filteredSentences, filteredTaggedSentences)
+def removeParentheses(sentence):
+    pCounter = 0
+    newSentence = ""
+    for i in range(len(sentence)):
+        if (sentence[i] == '(' or sentence[i] == '{'):
+            pCounter += 1
+        if (sentence[i] == ')' or sentence[i] == '}'):
+            pCounter -= 1
+        if (not(sentence[i] == '(' or sentence[i] == '{' or
+                sentence[i] == ')' or sentence[i] == '}') and pCounter == 0):
+            newSentence += sentence[i]
+    # Remove anything in slashes
+    newSentence = re.sub('\/([^\)]+)\/', '', newSentence)
+    return newSentence
 
 
-auxVerbs = ["have", "be", "do", "was", "is", "did", "has", "am", "are"]
-
-wasQuestion = "Was Dempsey born in Nacogdoches, Texas?"
-whereQuestion = "Where was Clint Dempsey born?"
-whoQuestion = "Who selected Clint Dempsey eighth overall in the 2004 MLS SuperDraft."
-
-txt = txt_ner.NER("set1/a1.txt")
-sentences = txt.sentences
-taggedSentences = txt.tagged_sentences
-
-# q = ner.NER(whereQuestion)
-q = ner.NER(whoQuestion)
-q.printTag()
-
-for wordTag in q.tagged_sentence:
-    word = wordTag[0]
-    lemWord = WordNetLemmatizer().lemmatize(word, 'v')
-    tag = wordTag[1]
-    # if tag == 'NNP':
-    #     taggedSentences = filterSentencesByWord(taggedSentences, word)
-    if (tag[:2] == 'VB') and (lemWord not in auxVerbs):
-        sentences, taggedSentences = filterSentencesByWord(sentences, taggedSentences, word)
-
-print(sentences)
-print(taggedSentences)
+def removeSemiColons(txt):
+    newTxt = []
+    for sentence in txt:
+        if ";" in sentence:
+            newSentences = sentence.split(";")
+            for i in range(len(newSentences)):
+                if newSentences[i][-1] != ".":
+                    newSentences[i] = newSentences[i] + "."
+                sentenceWords = newSentences[i].split()
+                sentenceWords[0] = sentenceWords[0].capitalize()
+                sentenceWords.append(" ")
+                newSentences[i] = " ".join(sentenceWords)
+            newTxt = newTxt + newSentences
+        else:
+            newTxt.append(sentence)
+    return newTxt
 
 
-# answer = "Clint Dempsey was born in Nacogdoches, Texas"
+class NER(object):
 
-# txt = ner.NER(answer)
-# txt.printTag()
+    def __init__(self, txt_path, q_root):
+        super(NER, self).__init__()
+
+        cur_path = os.path.dirname(__file__)
+        rel_path = '../data/' + txt_path
+        f_path = os.path.join(cur_path, rel_path)
+        with open(f_path, 'r') as f:
+            txt = f.readlines()
+
+        txt = [x.strip() for x in txt]
+
+        sentence_regex = "(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?)\s"
+        regex = re.compile(sentence_regex)
+
+        txt = [x for x in txt if regex.search(x)]
+        txt = removeSemiColons(txt)
+        txt = ''.join(str(elem) for elem in txt)
+        self.txt = txt
+        self.relevSentences = []
+        doc = nlp(unicode(txt))
+        for sent in doc.sents:
+            # print(sent.string)
+            # print("\n")
+            if (sent.root.lemma_ == q_root):
+                self.relevSentences.append(sent.string)
+                continue
+
+WORD = re.compile(r'\w+')
 
 
-# words = ["has", "have", "had", "having", "am", "is", "are",
-#          "was", "were", "being", "been", "does", "do", "did"]
+def get_cosine(vec1, vec2):
+    intersection = set(vec1.keys()) & set(vec2.keys())
+    numerator = sum([vec1[x] * vec2[x] for x in intersection])
 
-# for word in words:
-#     print(word + "-->" + WordNetLemmatizer().lemmatize(word, 'v'))
+    sum1 = sum([vec1[x]**2 for x in vec1.keys()])
+    sum2 = sum([vec2[x]**2 for x in vec2.keys()])
+    denominator = math.sqrt(sum1) * math.sqrt(sum2)
+
+    if not denominator:
+        return 0.0
+    else:
+        return float(numerator) / denominator
+
+
+def text_to_vector(text):
+    words = WORD.findall(text)
+    return Counter(words)
+
+
+class CosineSim(object):
+
+    def __init__(self, txt_path, question):
+        super(CosineSim, self).__init__()
+
+        cur_path = os.path.dirname(__file__)
+        rel_path = '../data/' + txt_path
+        f_path = os.path.join(cur_path, rel_path)
+        with open(f_path, 'r') as f:
+            txt = f.readlines()
+
+        txt = [x.strip() for x in txt]
+
+        sentence_regex = "(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?)\s"
+        regex = re.compile(sentence_regex)
+
+        txt = [x for x in txt if regex.search(x)]
+        txt = removeSemiColons(txt)
+        txt = ''.join(str(elem) for elem in txt)
+        self.txt = txt
+        self.sentSimCount = defaultdict(float)
+        questionVec = text_to_vector(question)
+        doc = nlp(unicode(txt))
+        for sent in doc.sents:
+            sentVector = text_to_vector(sent.string)
+            cosine = get_cosine(questionVec, sentVector)
+            self.sentSimCount[sent.string] = cosine
+        self.sortSentSimCount = sorted(self.sentSimCount,
+                                       key=self.sentSimCount.get,
+                                       reverse=True)
+        for i in xrange(10):
+            sent = self.sortSentSimCount[i]
+            print(sent, self.sentSimCount[sent])
+
+
+class CosineSim1(object):
+
+    def __init__(self, txt_path, question):
+        super(CosineSim1, self).__init__()
+
+        cur_path = os.path.dirname(__file__)
+        rel_path = '../data/' + txt_path
+        f_path = os.path.join(cur_path, rel_path)
+        with open(f_path, 'r') as f:
+            txt = f.readlines()
+
+        txt = [x.strip() for x in txt]
+
+        sentence_regex = "(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?)\s"
+        regex = re.compile(sentence_regex)
+
+        txt = [x for x in txt if regex.search(x)]
+        txt = removeSemiColons(txt)
+        txt = ''.join(str(elem) for elem in txt)
+        self.txt = txt
+        self.sentences = []
+        doc = nlp(unicode(txt))
+        for sent in doc.sents:
+            self.sentences.append(sent.string)
+        tfidf = TfidfVectorizer().fit_transforms(self.sentences)
+        tfidfQ = TfidfVectorizer().fit_transforms([question])
+        cosine_similarities = linear_kernel(tfidfQ, tfidf).flatten()
+        related_docs_indices = cosine_similarities.argsort()[:-5:-1]
+        for i in related_docs_indices:
+            print(self.sentences[related_docs_indices[i]])
+            
+
+
+# question = "Who selected Clint Dempsey eighth overall in the 2004 MLS SuperDraft?"
+# sentence = nlp(unicode(question))[2]
+
+# print([w.text for w in sentence.lefts])
+# print([w.text for w in sentence.rights])
+
+# sentence = nlp(unicode(question))
+# root = [w for w in sentence if w.head is w][0]
+# print root.text.lower()
+# print root.lemma_
+
+# print question
+
+# txt = CosineSim("set1/a1.txt", question)
+
+
+# question = "Where was Dempsey born?"
+# sentence = nlp(unicode(question))[2]
+
+# print([w.text for w in sentence.lefts])
+# print([w.text for w in sentence.rights])
+
+# sentence = nlp(unicode(question))
+# root = [w for w in sentence if w.head is w][0]
+# print root.text.lower()
+# print root.lemma_
+
+# print question
+
+# txt = NER("set1/a1.txt", root.lemma_)
+
+
+question = "Who did Beckham start dating in 1997?"
+sentence = nlp(unicode(question))
+
+for word in sentence:
+    print(word.text, word.pos_, word.dep_, word.head.text)
+
+root = [w for w in sentence if w.head is w][0]
+print root.text.lower()
+print root.lemma_
+
+print question
+
+if root.lemma_ != "be":
+    txt = NER("set1/a6.txt", root.lemma_)
+    for sent in txt.relevSentences:
+        print(sent)
+txt = CosineSim1("set1/a6.txt", question)
+
+
+
+
+# question = "Where was Donovan born?"
+# sentence = nlp(unicode(question))
+# for word in sentence:
+#     print(word.text, word.pos_, word.dep_, word.head.text)
