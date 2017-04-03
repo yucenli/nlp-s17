985368d69849966f4c15867cd6992299f725a9c8
Yucen Li <yucenl@andrew.cmu.edu>
2017-04-03 19:59:36 -0400

    different when questions

--- src/spacy_ner.py
+++ src/spacy_ner.py
@@ -33,7 +33,6 @@ class NER(object):
         f_path = os.path.join(cur_path, rel_path)
 
 
-        punct = {'.!?'}
         with open(f_path, 'r') as f:
             txt = f.readlines()
 
@@ -62,7 +61,7 @@ class NER(object):
                 print " "
 
         # Who questions
-        subject = ["he", "she", "they"]
+        subject = ["he", "she"]
 
         for sent in doc.sents:
             for i in range(0, len(sent)-1) :
@@ -86,6 +85,7 @@ class NER(object):
                     break
 
         # When questions
+        # Only works when original sentence is "In ____, blah blah."
         for sent in doc.sents:
             for i in range(0, len(sent)-1):
                 if (sent[i].ent_type_ == "DATE" and sent[i].pos_ != "ADJ"): 
@@ -113,4 +113,42 @@ class NER(object):
                     print ""
                     break
 
-txt = NER("set2/a1.txt")
+        for i in range(0, 10):
+            print ""
+
+        for sent in doc.sents:
+            for i in range(0, len(sent)-1):
+                if (sent[i].ent_type_ == "DATE"): 
+                    if (i > 0):
+                        front = Span(doc, sent.start, i+sent.start-1)
+                    else:
+                        font = []
+                    print sent
+                    valid = False
+                    while i < len(sent) - 1 and (sent[i].ent_type_ == "DATE" or sent[i].pos_ == "PUNCT"):
+                        i = i+1
+                        valid = True
+                    if valid:
+                        end = Span(doc, sent.start + i, sent.end-1)
+                        verb = sent[i]
+                        for t in sent.root.lefts:
+                            verb = t
+                        if verb.lemma_ == "be":
+                            final = "When was "
+                        else:
+                            final = "When did "
+                        for token in front:
+                            final = final + token.text + " "
+                        for token in end:
+                            if verb.lemma_ == "be" and token.lemma_ == "be":
+                                final = final
+                            elif verb.lemma_ != "be" and token == sent.root:
+                                final = final + sent.root.lemma_ + " "
+                            else:
+                                final = final + token.orth_ + " "
+                        print final[:-1] + "?"
+                        print ""
+                    break
+
+
+txt = NER("set3/a1.txt")
